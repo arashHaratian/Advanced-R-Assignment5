388f662500aef83c6df6159674027b5340efe5ce
Arash <45104106+arashHaratian@users.noreply.github.com>
2022-09-26 11:39:39 +0200

    temp API.R

--- API.R
+++ API.R
@@ -1,6 +1,12 @@
 # library("httr")
 library("jsonlite")
 
-URL <- "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=samtliga&org=&utformat=json&sort=sorteringsnamn&sortorder=asc&termlista="
-Response <- fromJSON(URL)
-df_Response <- data.frame(Response)
+
+
+dataset_query <- function(column_idx = NULL) {
+  URL <- "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=samtliga&org=&utformat=json&sort=sorteringsnamn&sortorder=asc&termlista="
+  response <- fromJSON(URL)
+  df_response <- data.frame(response)
+
+  df_raw <- response$personlista$person
+}
