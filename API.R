fca046f2a8ca08b8cd8e61a44636c6139ff7d329
ddiazron <112702667+ddiazron@users.noreply.github.com>
2022-09-26 11:08:56 +0200

    Changing to data.frame

--- API.R
+++ API.R
@@ -3,4 +3,4 @@ library("jsonlite")
 
 URL <- "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=samtliga&org=&utformat=json&sort=sorteringsnamn&sortorder=asc&termlista="
 Response <- fromJSON(URL)
-
+df_Response <- data.frame(Response)
